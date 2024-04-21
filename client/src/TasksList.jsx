61e19806617cfcdda22a5932f96dfa03ed4bcbe3
David Mostoller <dmostoller@gmail.com>
2024-04-21 04:46:32 -0400

    moved buttons to tasks

--- client/src/TasksList.jsx
+++ client/src/TasksList.jsx
@@ -14,11 +14,13 @@ function TasksList () {
     const tasksList = tasks.map((task) => {
         // console.log(task)
         return (
-            <Table.Row className="bg-gray-700 hover:bg-gray-900 dark:border-gray-700 dark:bg-gray-800">
-                <Table.Cell className="p-2"><Checkbox /></Table.Cell>
+            <Table.Row className="bg-gray-700 hover:bg-gray-900 border-gray-700 dark:bg-gray-800">
                 <Table.Cell><p className="text-xs text-secondary-500">{task.name}</p></Table.Cell>
                 <Table.Cell><p className="text-xs text-secondary-500">{task.description}</p></Table.Cell>
-                <Table.Cell><p className="text-xs text-secondary-500">{task.reward}</p></Table.Cell>
+                <Table.Cell><p className="text-xs text-secondary-500">+{task.reward}%</p></Table.Cell>
+                <Table.Cell className="p-2">
+                    <Button size="xs" gradientMonochrome="teal">Perform Task</Button>
+                </Table.Cell>
             </Table.Row>
         )
     })
@@ -28,11 +30,10 @@ function TasksList () {
            <div className="overflow-x-auto">
             <Table hoverable>
                 <Table.Head >
-                <Table.HeadCell className="p-2 bg-gray-700 dark:border-gray-700 dark:bg-gray-800">
-                </Table.HeadCell>
                 <Table.HeadCell className="bg-gray-700 dark:border-gray-700 dark:bg-gray-800"><p className="text-xs text-secondary-500">Task</p></Table.HeadCell>
                 <Table.HeadCell className=" bg-gray-700 dark:border-gray-700 dark:bg-gray-800"><p className="text-xs text-secondary-500">Description</p></Table.HeadCell>
                 <Table.HeadCell className=" bg-gray-700 dark:border-gray-700 dark:bg-gray-800"><p className="text-xs text-secondary-500">Reward</p></Table.HeadCell>
+                <Table.HeadCell className="p-2 bg-gray-700 dark:border-gray-700 dark:bg-gray-800"></Table.HeadCell>
                 </Table.Head>
                 <Table.Body className="divide-y">
                 {tasksList}
@@ -41,7 +42,7 @@ function TasksList () {
             </div>
             <div className="flex flex-wrap gap-2 m-4 justify-center" >
             <Button.Group>
-                <Button gradientMonochrome="teal">Perform Task</Button>
+
                 <Button gradientMonochrome="teal">Log New Task</Button>
                 <Button gradientMonochrome="teal">Refresh Task List</Button>
             </Button.Group>
